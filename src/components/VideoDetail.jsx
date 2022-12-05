5a66fcd8495ec658c0c820ba3c52145d50304e79
Dmitry Gerasimuk <dmitrygerasimuk@gmail.com>
2022-12-05 14:19:58 +0300

    asdasd

---
+++ src/components/VideoDetail.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import ReactPlayer from "react-player";
+import { Typography, Box, Stack } from "@mui/material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+
+import { Videos, Loader } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+
+const VideoDetail = () => {
+  const [videoDetail, setVideoDetail] = useState(null);
+  const [videos, setVideos] = useState(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`videos?part=snippet,statistics&id=${id}`)
+      .then((data) => setVideoDetail(data.items[0]))
+
+    fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`)
+      .then((data) => setVideos(data.items))
+  }, [id]);
+
+  if(!videoDetail?.snippet) return <Loader />;
+
+  const { snippet: { title, channelId, channelTitle }, statistics: { viewCount, likeCount } } = videoDetail;
+
+  return (
+    <Box Height="100%">
+     
+            <ReactPlayer  url={`https://www.youtube.com/watch?v=${id}`} playing
+config={{
+youtube: {
+playerVars: {
+controls: 0,
+modestbranding: 1,
+},
+},
+}} className="react-player"  />
+          
+    
+    </Box>
+  );
+};
+
+export default VideoDetail;
---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': '1304e60d2cmshce3a356c499eea8p184bd7jsn7f0c10de606a',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
